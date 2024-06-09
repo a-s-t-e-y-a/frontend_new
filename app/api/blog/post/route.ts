1e569d309cead6c612e4c31139dfea4527f2554a
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-06-09 15:39:12 +0530

    ✨ New feature blog post api done

--- app/api/blog/post/route.ts
+++ app/api/blog/post/route.ts
@@ -1,3 +1,58 @@
-export async function GET(){
-  return new Response("Hello World")
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client/extension";
+import { z } from "zod";
+const prisma = new PrismaClient();
+
+const Blog = z.object({
+  title: z.string().min(1, "Title is required"),
+  slug: z.string().min(1, "Slug is required"),
+  content: z.string().min(1, "Content is required"),
+  url: z.string().min(1, "URL is required"),
+  metaDescription: z.string().min(1, "Meta description is required"),
+  metaKeywords: z.string().min(1, "Meta keywords are required"),
+});
+export async function POST(req: Request) {
+  let resposne = NextResponse.next();
+  try {
+    const token = resposne.cookies.get("token");
+    const data = Blog.parse(await req.json());
+    if (!token) {
+      return new Response(
+        JSON.stringify({
+          status: 404,
+          data: null,
+          message: "Token not provided",
+        }),
+        {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const data_ = await prisma.blog.create({
+      data: {
+        ...data,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+    return new Response(
+      JSON.stringify({
+        status: 404,
+        data: data_,
+        message: "Blog created successfully",
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (err) {
+    return Response.json(err);
+  }
 }

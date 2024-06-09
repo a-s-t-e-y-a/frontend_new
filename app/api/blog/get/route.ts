905851b92fe6e671ba8a96322ea92e5c451e3dcc
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-06-09 16:06:47 +0530

    🐛 Bug fix bug fix in blog get all route.ts

--- app/api/blog/get/route.ts
+++ app/api/blog/get/route.ts
@@ -20,29 +20,3 @@ export async function GET() {
     );
   }
 }
-
-export async function GET_BY_ID(req, { params }) {
-  try {
-    const blog = await prisma.post.findUnique({ where: { url: params.id } });
-    if (!blog) {
-      return NextResponse.json(
-        {
-          status: 404,
-          data: null,
-          message: "Blog not found",
-        },
-        { status: 404 }
-      );
-    }
-    return NextResponse.json({ status: 200, data: blog }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json(
-      {
-        status: 500,
-        data: null,
-        message: "An error occurred while fetching the blog",
-      },
-      { status: 500 }
-    );
-  }
-}

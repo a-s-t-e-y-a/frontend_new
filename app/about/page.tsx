0f0a1bfc1a5597ce7944204ed57419b04dae56b9
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-06-20 21:25:59 +0530

    ✨ New feature dfjqasioj

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import React from 'react'
 import { Menu, X, MapPin } from 'lucide-react'
 import { Navbar } from '../components/navbar'
@@ -9,8 +8,6 @@ import Image from 'next/image'
 import { DirectionAwareHover } from '../components/ui/direction-aware-hover'
 import img2 from '@/public/visions-768x512.jpg'
 import img3 from '@/public/about_us.jpg'
-
-
 
 export default function About() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
@@ -22,45 +19,39 @@ export default function About() {
   return (
     <div className='bg-white text-gray-600'>
       <Navbar></Navbar>
-      <Image src={img3} alt=''  className='m-auto w-[100%] h-[400px] object-cover'></Image>
+      <Image src={img3} alt='' className='m-auto w-[100%] h-[400px] object-cover'></Image>
+      <div className='max-w-[1200px] mx-auto'>
 
-      <div className="mx-auto w-4/5 px-4 ">
-        {/* Hero Map */}
-        <div className="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
-          {/* <div className="max-w-max rounded-full border bg-gray-50 p-1 px-3">
-            <p className="text-xs font-semibold leading-normal md:text-sm">About the company</p>
-          </div> */}
-          <p className="text-3xl font-bold text-gray-900 md:text-5xl md:leading-10">
-            ABOUT US
-          </p>
-          <p className="max-w-4xl text-base text-gray-600 md:text-xl">
+      <div className="my-10 flex flex-col lg:flex-row items-center justify-between lg:px-16 lg:gap-20 space-x-2xl">
+        <div className="w-full lg:w-1/2">
+          {/* <Image src={img1} alt="why" unoptimized height={400} width={400} className="rounded-lg" /> */}
+          {/* <h1 className="text-2xl my-6 font-bold text-black text-center lg:text-left">Why Choose Us?</h1> */}
+          <p className="text-justify my-10">
             We, at eVC-Tech, are a team of professionals with experience in and exposure to all facets of content solutions and publishing services. Our services range from content production to transformation, enrichment, and even content development in some cases for clients as diverse as publishers, societies, government organizations, and data aggregators, among others. We draw our clientele from all parts of the globe.
           </p>
         </div>
-        <div className="w-full space-y-4">
-          <div className='flex justify-center'>
-
-
-          </div>
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
         </div>
+      </div>
 
-        <hr className="my-4" />
- <div className="min-h-screen flex flex-col lg:flex-row items-center justify-center  lg:px-16  lg:gap-20">
-  <div className="w-full lg:w-1/2">
-    <h1 className="text-2xl my-6 font-bold text-black text-center lg:text-left">Why Choose Us?</h1>
-    <p className="text-justify">
-      In today’s content marketplace, the choices seem endless. Many companies boast of efficiencies derived from ideal-case scenarios filled with technological jargon. However, reality often differs from the ideal. At eVC-Tech, we recognize that clients need solutions that add genuine value to their processes and products, going beyond what off-the-shelf options can offer. There’s a significant gap between the bespoke needs of clients and the one-size-fits-all offerings from big vendors. Bridging this gap is our mission.
-      <br /><br />
-      Customization is at the heart of our services. We don’t push generic solutions. Instead, we take the time to understand each client’s unique requirements, considering their evolving markets and business models. We approach every project with an open mind, designing our processes and services to align with our clients’ objectives. By integrating cutting-edge tools and leveraging the rich knowledge of our subject-matter experts (SMEs), we not only deliver quality products on time but also enhance their overall value.
-    </p>
-  </div>
-  <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
-    <Image src={img1} alt="why" unoptimized height={400} width={400} className="rounded-lg" />
-  </div>
-</div>
+      {/* <hr className="my-4" /> */}
 
+      <div className="my-10 flex flex-col lg:flex-row items-center justify-between lg:px-16 lg:gap-20 space-x-2xl">
+        <div className="w-full lg:w-1/2">
+          <h1 className="text-2xl my-6 font-bold text-black text-center lg:text-left">Why Choose Us?</h1>
+          <p className="text-justify">
+            In today’s content marketplace, the choices seem endless. Many companies boast of efficiencies derived from ideal-case scenarios filled with technological jargon. However, reality often differs from the ideal. At eVC-Tech, we recognize that clients need solutions that add genuine value to their processes and products, going beyond what off-the-shelf options can offer. There’s a significant gap between the bespoke needs of clients and the one-size-fits-all offerings from big vendors. Bridging this gap is our mission.
+            <br /><br />
+            Customization is at the heart of our services. We don’t push generic solutions. Instead, we take the time to understand each client’s unique requirements, considering their evolving markets and business models. We approach every project with an open mind, designing our processes and services to align with our clients’ objectives. By integrating cutting-edge tools and leveraging the rich knowledge of our subject-matter experts (SMEs), we not only deliver quality products on time but also enhance their overall value.
+          </p>
+        </div>
+          <Image src={img1} alt="why" className='w-full sm:w-[400px] aspect-square my-5 sm:my-0'/>
+      </div>
+   
+      
 
-        <hr className='my-20'></hr>
+      {/* ... rest of the code ... */}
+      <hr className='my-20'></hr>
 
         <div className="max-w-4xl mx-auto py-10">
           <h2 className="text-center text-2xl font-bold mb-5">Our team has extensive experience in delivering the highest level of quality across a range of services, such as:</h2>
@@ -75,42 +66,42 @@ export default function About() {
         </div>
 
         <div className="mx-auto max-w-7xl">
-  <div className="prose mx-auto">
-    <div className="flex flex-wrap gap-10 justify-center items-center my-20">
-      <Image src={img2} className='aspect-square object-contain w-[450px]'  alt=''></Image>
-      <div className="sm:w-1/2">
-  <h2 className="font-bold text-center my-10 text-2xl text-gray-800">Vision & Mission</h2>
-        <h3 className="text-lg font-bold">Vision:</h3>
-        <p>To transform the content and publishing services landscape with innovative, client-focused solutions that inspire and drive success.</p>
+          <div className="prose mx-auto">
+            <div className="flex flex-wrap gap-10 justify-center items-center my-20">
+              <Image src={img2} className='aspect-square object-contain w-[450px]' alt=''></Image>
+              <div className="sm:w-1/2">
+                <h2 className="font-bold text-center my-10 text-2xl text-gray-800">Vision & Mission</h2>
+                <h3 className="text-lg font-bold">Vision:</h3>
+                <p>To transform the content and publishing services landscape with innovative, client-focused solutions that inspire and drive success.</p>
 
-        <h3 className="text-lg font-bold">Mission:</h3>
-        <p>We are dedicated to delivering unparalleled content and publishing services by placing our clients’ needs at the forefront. Through continuous learning and adaptive strategies, we ensure the highest quality, efficiency, and satisfaction. Our mission is to build lasting partnerships that thrive on trust, excellence, and mutual success.</p>
-      </div>
-    </div>
+                <h3 className="text-lg font-bold">Mission:</h3>
+                <p>We are dedicated to delivering unparalleled content and publishing services by placing our clients’ needs at the forefront. Through continuous learning and adaptive strategies, we ensure the highest quality, efficiency, and satisfaction. Our mission is to build lasting partnerships that thrive on trust, excellence, and mutual success.</p>
+              </div>
+            </div>
 
-    <h2 className="text-center text-2xl font-bold my-10">Our Core Values:</h2>
-    <ul className="list-disc grid gap-5 sm:grid-cols-2 mx-auto">
-      <li className="">
-        <strong>Innovation:</strong> We embrace cutting-edge technologies and creative approaches to provide unique solutions.
-      </li>
-      <li className="">
-        <strong>Client-Centricity:</strong> Our clients’ success and satisfaction are the benchmarks of our achievements.
-      </li>
-      <li className="">
-        <strong>Excellence:</strong> We commit to the highest standards of quality and reliability in every project.
-      </li>
-      <li className="">
-        <strong>Collaboration:</strong> We believe in the power of teamwork, both within our company and with our clients.
-      </li>
-      <li className="">
-        <strong>Continuous Improvement:</strong> We foster a culture of perpetual learning and enhancement of our skills, processes, and technologies.
-      </li>
-    </ul>
-  </div>
-</div>
-<br></br>
+            <h2 className="text-center text-2xl font-bold my-10">Our Core Values:</h2>
+            <ul className="list-disc grid gap-5 sm:grid-cols-2 mx-auto">
+              <li className="">
+                <strong>Innovation:</strong> We embrace cutting-edge technologies and creative approaches to provide unique solutions.
+              </li>
+              <li className="">
+                <strong>Client-Centricity:</strong> Our clients’ success and satisfaction are the benchmarks of our achievements.
+              </li>
+              <li className="">
+                <strong>Excellence:</strong> We commit to the highest standards of quality and reliability in every project.
+              </li>
+              <li className="">
+                <strong>Collaboration:</strong> We believe in the power of teamwork, both within our company and with our clients.
+              </li>
+              <li className="">
+                <strong>Continuous Improvement:</strong> We foster a culture of perpetual learning and enhancement of our skills, processes, and technologies.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <br></br>
+        </div>
         <Footer></Footer>
-      </div>
     </div>
   )
 }
--- app/content-transformation/page.tsx
+++ app/content-transformation/page.tsx
@@ -14,10 +14,10 @@ export default function ContentTransformationPage() {
       <Image src={img1} alt="" className="w-[100%]"></Image>
      
 
-<div className="flex flex-col justify-center items-center ">
+<div className="flex flex-col justify-center items-center my-12">
 
-        <h1 className="text-purple-800 mb-5  text-3xl sm:text-6xl font-extrabold mt-5 sm:my-10">Content Transformation</h1>
-        <h3 className="text-base w-full sm:w-3/4 m-auto text-gray-500 sm:text-center">Technology has been evolving continuously and so have the standards to store and exchange content. The trend has created an ever-growing need to transform content from one standard to another.</h3>
+        <h1 className="text-purple-800 mb-5  text-3xl sm:text-6xl font-extrabold mt-8 sm:my-10">Content Transformation</h1>
+        <h3 className="text-base w-full my-0 sm:my-8 sm:w-3/4 px-2 sm:px-6 m-auto text-gray-500 sm:text-center">Technology has been evolving continuously and so have the standards to store and exchange content. The trend has created an ever-growing need to transform content from one standard to another.</h3>
         {/* <Mouse className="text-blue-400 scale-150 m-auto my-60"></Mouse> */}
 </div>
      
@@ -26,28 +26,28 @@ export default function ContentTransformationPage() {
 
       <div className="grid grid-cols-1 md:grid-cols-12 gap-8 px-4 py-16 mx-auto max-w-7xl">
   <div className="col-span-full md:col-span-8 flex flex-col justify-center items-center space-y-8">
-    <h2 className="text-xl md:text-4xl font-bold text-pretty mb-4">Content Transformation Services: Stay Relevant in a Competitive Market</h2>
+    <h2 className="text-xl md:text-2xl font-bold text-pretty mb-4">Content Transformation Services: Stay Relevant in a Competitive Market</h2>
     <p className="text-lg md:text-xl text-gray-700 leading-relaxed text-pretty">
       Every organization that deals in content may require content transformation services to remain relevant in this competitive market. We have sound experience in converting any document format or media to any other (e.g., XML), structuring unstructured data, digitizing legacy content, extracting information from images, and embedding metadata for archiving/retrievals, and more.
     </p>
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div className="flex flex-col space-y-2">
-        <h3 className="text-2xl font-bold">Print-only Formats & Digital</h3>
+        <h3 className="text-xl font-bold">Print-only Formats & Digital</h3>
         <ul className="list-disc pl-4 text-gray-700">
           <li>Hard copy books/magazines/newspapers: Scan, OCR, double key-in</li>
           <li>Scanning negative films/photographs and enhancements</li>
         </ul>
       </div>
       <div className="flex flex-col space-y-2">
-        <h3 className="text-2xl font-bold">One Digital Standard to Another</h3>
+        <h3 className="text-xl font-bold">One Digital Standard to Another</h3>
         <ul className="list-disc pl-4 text-gray-700">
           <li>Transforming from legacy Word/SGML/XML standards to the latest BITS/JATS/DITA/proprietary standards</li>
           <li>Creating graphs and reports from Excel/Access/Proprietary data sources</li>
         </ul>
       </div>
       <div className="flex flex-col space-y-2">
-        <h3 className="text-2xl font-bold">Platform Specific Conversion</h3>
+        <h3 className="text-xl font-bold">Platform Specific Conversion</h3>
         <ul className="list-disc pl-4 text-gray-700">
           <li>Conversion from legacy print file standards to a modern Print-on-Demand (POD) standard</li>
         </ul>

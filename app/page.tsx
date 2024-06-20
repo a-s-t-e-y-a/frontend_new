c02f6a51b13f20c813e583cd2071606b229e474d
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-06-20 12:57:11 +0530

    🔧 Maintenance bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ const Card = ({ imageUrl, title, description, href }:any) => (
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
       <Link href={href}>
         <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-violet-500 rounded-lg hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Read more
+          Learn More
           <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
           </svg>
